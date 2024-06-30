32aa82f90a388e880742f8c46cd8f129d946f0f2
kamal <kamal@gmail.com>
2024-06-30 11:14:06 +0300

    02-init

---
+++ src/components/PostList.jsx
@@ -0,0 +1,26 @@
+import {
+  Table,
+} from "react-bootstrap";
+import PostListItem from "./PostListItem";
+
+const PostList = ({ data, loading, error }) => {
+
+  
+
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th style={{ width: "70%" }}>Title</th>
+          <th style={{ width: "10%" }}></th>
+        </tr>
+      </thead>
+      <tbody>
+       <PostListItem data={data} loading={loading} error={error} />
+      </tbody>
+    </Table>
+  )
+}
+
+export default PostList
